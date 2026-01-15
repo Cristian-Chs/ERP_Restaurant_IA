20bbf8bdab100cd90260d4b89da833646d73259c
Cristian <cristian.adrian.chirinos2@gmail.com>
2026-01-15 19:44:52 -0400

    Fix: correct API url and start bot process

--- reactproject/src/api/axios.js
+++ reactproject/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:8000/api',
 });
 
 API.interceptors.request.use((config) => {
